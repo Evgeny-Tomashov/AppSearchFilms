c3bf5638fdcf5bffb4c85c5f3ce2b5fb0fd82de0
Evgeny-Tomashov <tomashov86@gmail.com>
2023-04-25 20:19:21 +0800

    Добавлено выполнение задания 20.8
    Добавлено выполнение задания 20.8

--- app/src/main/java/com/devtomashov/appsearchfilms/MainActivity.kt
+++ app/src/main/java/com/devtomashov/appsearchfilms/MainActivity.kt
@@ -2,10 +2,31 @@ package com.devtomashov.appsearchfilms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClickToastMenu (view: View) {
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToastFavorites (view: View) {
+        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToastWatchLater (view: View) {
+        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToastSelections (view: View) {
+        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+    }
+    fun onClickToastSettings (view: View) {
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
     }
 }
