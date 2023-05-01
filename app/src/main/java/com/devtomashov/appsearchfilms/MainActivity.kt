98a3d168e56acf21249a319ef6a3eb5cda0a118d
Evgeny-Tomashov <tomashov86@gmail.com>
2023-05-01 19:13:58 +0800

    homework 21.8
    Добавлено выполнение задания 21.8

--- app/src/main/java/com/devtomashov/appsearchfilms/MainActivity.kt
+++ app/src/main/java/com/devtomashov/appsearchfilms/MainActivity.kt
@@ -2,10 +2,69 @@ package com.devtomashov.appsearchfilms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.devtomashov.appsearchfilms.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.menu_settings_title),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.menu_favorites_title),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                R.id.watch_later -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.menu_watch_later_title),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                R.id.selections -> {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.menu_selections_title),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
+
+
+
+
