5cca9d90271b275be01973cccf3c7e6b006a1592
Evgeny-Tomashov <tomashov86@gmail.com>
2023-05-28 19:22:41 +0800

    Homework_25.7
    Добавлено выполнение задания 25.7 (разделение на фрагменты, рефаторинг).

---
+++ app/src/main/java/com/devtomashov/appsearchfilms/DetailsFragment.kt
@@ -0,0 +1,47 @@
+package com.devtomashov.appsearchfilms
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.devtomashov.appsearchfilms.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private var detailBinding: FragmentDetailsBinding? = null
+    private val binding get() = detailBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        detailBinding = FragmentDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFilmsDetails()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        detailBinding = null
+    }
+
+    private fun setFilmsDetails() {
+        //Получаем наш фильм из переданного бандла
+        val film = arguments?.get("film") as Film
+
+        //Устанавливаем заголовок
+        detailBinding?.detailsToolbar?.title = film.title
+        //Устанавливаем картинку
+        detailBinding?.detailsPoster?.setImageResource(film.poster)
+        //Устанавливаем описание
+        detailBinding?.detailsDescription?.text = film.description
+    }
+}
