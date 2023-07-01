0828ea0902a836f09bea7108d5b6e82ab2d52fed
Evgeny-Tomashov <Tomashov86@gmail.com>
2023-07-01 12:36:16 +0800

    homework_29.7
    Добавлено:
    1. Проявление фрагмента через Circular Reveal Animation.
    2. Два фрагмента под «Посмотреть позже» и «Подборки», анимация их появления.
    3. Splash Screen (экран приветствия) с векторной анимацией.

--- app/src/main/java/com/devtomashov/appsearchfilms/HomeFragment.kt
+++ app/src/main/java/com/devtomashov/appsearchfilms/HomeFragment.kt
@@ -7,14 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devtomashov.appsearchfilms.databinding.FragmentHomeBinding
+import java.util.Locale
 
 class HomeFragment : Fragment() {
-    private var bindingFrag: FragmentHomeBinding? = null
-    private val binding1 get() = bindingFrag!!
+    private var bindingHome: FragmentHomeBinding? = null
+
+    private val binding get() = bindingHome!!
 
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
+
 
     val filmsDataBase = listOf(
         Film(
@@ -78,17 +82,52 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        bindingFrag = FragmentHomeBinding.inflate(inflater, container, false)
-        val view = binding1.root
+        bindingHome = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        AnimationHelper.performFragmentCircularRevealAnimation(
+            binding.homeFragmentRoot,
+            requireActivity(),
+            1
+        )
+
+        bindingHome?.searchView?.setOnClickListener {
+            bindingHome?.searchView?.isIconified = false
+        }
+
+        //Подключаем слушателя изменений введенного текста в поиска
+        bindingHome?.searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //Этот метод отрабатывает при нажатии кнопки "поиск" на софт клавиатуре
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            //Этот метод отрабатывает на каждое изменения текста
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Если ввод пуст то вставляем в адаптер всю БД
+                if (newText.isEmpty()) {
+                    filmsAdapter.addItems(filmsDataBase)
+                    return true
+                }
+                //Фильтруем список на поискк подходящих сочетаний
+                val result = filmsDataBase.filter {
+                    //Чтобы все работало правильно, нужно и запрос, и имя фильма приводить к нижнему регистру
+                    it.title.lowercase(Locale.getDefault())
+                        .contains(newText.lowercase(Locale.getDefault()))
+                }
+                //Добавляем в адаптер
+                filmsAdapter.addItems(result)
+                return true
+            }
+        })
+
         //находим наш RV
-        bindingFrag?.mainRecycler?.apply {
-            //Инициализируем наш адаптер в конструктор передаем анонимно инициализированный интерфейс
+        bindingHome?.mainRecycler?.apply {
             filmsAdapter =
                 FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {
                     override fun click(film: Film) {
@@ -99,18 +138,19 @@ class HomeFragment : Fragment() {
             adapter = filmsAdapter
             //Присвои layoutmanager
             layoutManager = LinearLayoutManager(requireContext())
-            //Применяем декораcтор для отступов
+            //Применяем декоратор для отступов
             val decorator = TopSpacingItemDecoration(8)
             addItemDecoration(decorator)
         }
+
         //Кладем нашу БД в RV
         filmsAdapter.addItems(filmsDataBase)
+
     }
 
     override fun onDestroyView() {
+        bindingHome = null
         super.onDestroyView()
-        bindingFrag = null
     }
 
 }
-
